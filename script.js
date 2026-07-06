4c11c072ff52a24c09facdb9608e176414b5c27a
kjunsu0120 <kjunsu0120@gmail.com>
2026-07-06 10:46:21 +0900

    Update script.js
    Create script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+  const askButton = document.getElementById("askButton");
+  const questionInput = document.getElementById("questionInput");
+  const questionList = document.getElementById("questionList");
 
+  askButton.addEventListener("click", function () {
+    const questionText = questionInput.value;
+
+    if (questionText.trim() === "") {
+      alert("질문을 입력해주세요!");
+      return;
+    }
+
+    const newQuestion = document.createElement("li");
+    newQuestion.textContent = questionText;
+
+    questionList.appendChild(newQuestion);
+
+    questionInput.value = "";
+  });
